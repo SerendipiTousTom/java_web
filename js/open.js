47c76f9f3eb57841fe52a61958c0fa0384204282
guohongyang20 <119857059+guohongyang20@users.noreply.github.com>
2022-12-10 15:48:12 +0800

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,21 @@
+document.addEventListener('keydown', function (event) {
+        if ((event.ctrlKey === true || event.metaKey === true)
+            && (event.which === 61 || event.which === 107
+                || event.which === 173 || event.which === 109
+                || event.which === 187 || event.which === 189)) {
+            event.preventDefault();
+        }
+    }, false);
+    // Chrome IE 360
+window.addEventListener('mousewheel', function (event) {
+        if (event.ctrlKey === true || event.metaKey) {
+            event.preventDefault();
+        }
+    }, { passive: false });
+
+    //firefox
+window.addEventListener('DOMMouseScroll', function (event) {
+        if (event.ctrlKey === true || event.metaKey) {
+            event.preventDefault();
+        }
+    }, { passive: false });
